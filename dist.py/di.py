a4a08c3cf79b685795bcae1f670bc504e0d8eee4
harshita <harshita@mail.com>
2022-07-16 13:40:28 +0530

    intial commit

---
+++ dist.py/di.py
@@ -0,0 +1,7 @@
+map_ = {
+    'fname':'harshita',
+    'lname':'soni',
+    'company':'regex'
+
+}
+print(map_.get ('company','not found'))
---
+++ harshi.py
@@ -0,0 +1,43 @@
+
+
+
+# lst = [1,22,3,4, ['a','b','c'] , 'hello']
+# print (lst [3])
+# print (lst[4][1])
+# a = [1,2,3,4]
+# b = [5,6,7,8]
+# a.append (b)
+# print (a)
+#st = set ([1,2,3,4])
+# map_ = {
+#     'fname':'harshita',
+#     'lname':'soni',
+#     'company':'regex'
+
+# }
+# print (map_.get ('company','not found'))
+
+# print(map_.pop('company'))
+# lst = [1,2,3,4,5,6,7,8,9,10]
+# for i in lst [2:9] :
+#     print (i)
+
+# for i in [1,2,3]:
+#     for j in ['a','b','c'] :
+#         print (i,j)
+# a = int(input("enter any number"))
+# if a>1:
+
+#  for i in range (2 , int(a/2)+1):
+#     if (a % i ) == 0:
+#         print ("it is  not a prime number" )
+#         break  
+#     else: 
+#         print (" it is a prime number")
+# else:
+#     print ("no")        
+         
+
+        
+
+
