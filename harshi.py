1ea6c3b3317c9ccb646c03a192965531326dabd6
harshita <harshita@mail.com>
2022-07-17 13:20:34 +0530

    added changes and prolem solving

--- harshi.py
+++ harshi.py
@@ -35,9 +35,43 @@
 #     else: 
 #         print (" it is a prime number")
 # else:
-#     print ("no")        
-         
+#     print ("no")  
+# n = input ("enter any string")
+# for i in n:
+#     if ord(i) % 2 ==0:
+#         print (i, 'is in even number posisition')
 
-        
+# n = int (input ("enter how many number you want in a list "))
+# lst = []
+# for i in range (n):
+#     m=(input ("enter n numbers"))
+#     lst.append(m)
+# print (lst)
+
+
+n = int(input ("enter any number"))
+dist = {
+ 'str': [],
+ 'int': [],
+ 'float':[],
+ 'bool': []
+} 
+for i in range (n):
+    dt = input('enter datatype:')
+    value = input('enter value')
+    if dt == 'str':
+        dist['str'].append(value)
+    elif  dt =='int':
+        dist['int'].append(int(value))
+    elif dt == 'float' :
+        dist['float'].append(float(value)) 
+    else:
+        print('please initialise a empty list for {dt}'.format(dt))
+print(dist)    
+
+
+
+
+
 
 
