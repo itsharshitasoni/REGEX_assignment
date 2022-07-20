5204ebe9b2df5b0030c5ceabe24d2e6b69b4c388
harshita <harshita@mail.com>
2022-07-20 18:36:37 +0530

    added mroe files

--- dist.py/di.py
+++ dist.py/di.py
@@ -1,7 +1,32 @@
-map_ = {
-    'fname':'harshita',
-    'lname':'soni',
-    'company':'regex'
+# map_ = {
+#     'fname':'harshita',
+#     'lname':'soni',
+#     'company':'regex'
 
-}
-print(map_.get ('company','not found'))
+# }
+# print(map_.get ('company','not found'))
+# n = input ("enter any input")
+# for i in n:
+#       if((i>= 'a' and i<='z' and i>='A' or  i<='Z' and i>='0' or i<='9' and i<='$' or i>='#')):
+#          print ("you have fulfilled all the requirement")
+#          break
+#       else:
+#          print("enter the required format")
+#          break\
+# n = input("enter any string")
+# for i in range (n):
+# n.split()
+
+
+n = input('enter any number')
+# sum = 1
+# total = 0
+for i in range (n): 
+    op = input ('please enter opration string: ')
+    op_list = op.append()
+    print(op_list)
+
+   
+
+   
+    
--- harshi.py
+++ harshi.py
@@ -71,16 +71,6 @@
 
 # from tkinter import N
 
-
-# n = input ("enter any input")
-# # for i in n: 
-# if ((n >= 'a' and n <='z' or n >='A' and n <='Z' or n >='0' and n <='9')):
-#         print ("you have entered all the requirement")
-# else:
-#         print("enter the req format")    
-    
-
-
 # n = int(input('how many test cases:'))
 # sum = 1
 # total = 0
@@ -131,17 +121,135 @@
 #     print ("it is a plandrome")
 # else:
 #     print ("it is not a palandrome")    
-n = int(input("how many cases are their:"))
-p =''
-for i in range(n):
-    m =int(input ("please enter any number"))
-    str=chr(m)
-    p = str + p
-print (p)
+# n = int(input("how many cases are their:"))
+# p =''
+# for i in range(n):
+#     m =int(input ("please enter any number"))
+#     str=chr(m)
+#     p = str + p
+# print (p)
+ 
+# for i in range(3):
+#     p = int(0)
+#     q = int(0)
+#     r = int(0)
+#     s = int(0)
+#     psd = input("Enter a password ")
+    
+    # if len(psd) > 0 and len(psd) <= 16:
+            
+    #     for j in range(0,len(psd)):
+                
+    #         if ord(psd[j]) >= 48 and ord(psd[j]) <= 57:
+    #             p = int(1)  # numbers
+    #         if ord(psd[j]) >= 65 and ord(psd[j]) <= 90:
+    #             q = int(1)  # Capital Alphabets
+    #         if ord(psd[j]) >= 97 and ord(psd[j]) <= 122:
+    #             r = int(1)  # small alphabets
+    #         if ord(psd[j]) >= 33 and ord(psd[j]) <= 47 or ord(psd[j])  == 64 :
+    #             s = int(1)   # special symbols
+    # print (p,q,r,s)
+    # if p == 1 and q == 1 and r == 1 and s == 1:
+    #     print('valid password')
+    #     break
+    # else:
+    #     continue
+
+# n = input("enter any string")
+# m = n.lower()
+
+# dist ={}
+# for i in m:
+#     if i in dist:
+#         dist[i] += 1 
+#     else:
+#       dist[i] = 1     
+# print(dist)
 
 
+# from setuptools import Command
 
 
+# m=[]
+# while True:
+#     command = input ()
+#     op, value = Command.split()
+#     if command[1] == 'stop':
+#         break
+#     elif command == 'push':
+#         lst= m.append(n)
+# print(n)
+
+# m={}
+# while True:
+#     inp=input("enter any string")
+#     if inp == 'stop':
+#          break
+#     else:
+#         inp =inp.split
+#         m[0] =='m'
+# table function                                                                                                            
+# n= int(input("enter any number"))
+# def table(n):
+#     for i in range(1,11):
+#         print (n*i)
+# output=table(n)
+
+# list=['1','2','3']
+# def hh (list):
+#     print(list)
+# out=hh(list)        
+
+# n=input("enter any string")
+# def harsh(n):
+#  for i in n:
+#     if i ==i.upper():
+#         return("true")
+#     else:
+#         return("False")  
+#         break
+# out= harsh(n)
+# print(out)        
+
+# n = input("enter any string")
+# def ordo (n):
+#     sum=0
+#     for i in n:
+#     #i =i.split(' : :1')
+#       sum+= ord(i)
+#     return sum
+# print(ordo (n))
+
+# def hh (input_string):
+#     return sum(map(ord , input_string))
+#  
+# username=input("enter the username")
+# password = input ("enter the password")
+# def generate (username ,password):
+#   str1= username[0:4] + password[-4:]
+#   print(str1)
+# generate(username,password)
+
+# n = input("enter any string")
+# def fun (n):
+#     total=0
+#     for i in range(len(n)-1):
+#         if ord(n[i]) +1 == ord(n[i+1]):
+#             total +=1
+#     return total
+# print(fun (n))           
+ 
+    
+lst=[1,3,100,9,1120]
+def p (lst):
+    for i in lst:
+        if (i% 2==0):
+            print(i)
+        else:
+            print("no")
+p(lst)
+
+         
 
 
 
